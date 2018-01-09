bc4bcab29c375d733a6e0a0ba0824964d547a20d
mcraigie <max.craigie@gmail.com>
2018-01-09 18:03:14 +1100

    add basic db seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+File.read("./data/types.txt").each_line do |line|
+  Type.create!(name: line.strip)
+end
+
+File.read("./data/divisions.txt").each_line do |line|
+  Division.create!(name: line.strip)
+end
+
+File.read("./data/models.txt").each_line do |line|
+  Model.create!(number: line.strip)
+end
+
+File.read("./data/suburbs.txt").each_line do |line|
+  Suburb.create!(name: line.strip)
+end
+
+CSV.foreach("./data/subset_outdoor_furniture.csv", :headers => true) do |row|
+  row = row.to_hash
+  row['type'] = Type.find_by(name: row['type'])
+  row['division'] = Division.find_by(name: row['division'])
+  row['model'] = Model.find_by(number: row['model'])
+  row['suburb'] = Suburb.find_by(name: row['suburb'])
+  Piece.create!(row)
+end
