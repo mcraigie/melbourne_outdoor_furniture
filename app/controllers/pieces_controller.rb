c3fafe37c21a2b105866b48d6e298b08b0909677
mcraigie <max.craigie@gmail.com>
2018-01-11 19:57:33 +1100

    remove unused scaffolding

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,5 +1,5 @@
 class PiecesController < ApplicationController
-  before_action :set_piece, only: %i[show edit update destroy]
+  before_action :set_piece, only: %i[show]
 
   # GET /pieces
   # GET /pieces.json
@@ -17,7 +17,7 @@ class PiecesController < ApplicationController
     associated_models = %i[type division suburb model]
     sortable_fields = %w[description gis_identifier condition evaluated created_at updated_at]
 
-    associated_models.each do |model|
+    associated_models.map { |m| "#{m.to_s}_id"}.each do |model|
       scope = scope.where(model => search_params[model]) if search_params[model].present?
     end
 
@@ -38,81 +38,11 @@ class PiecesController < ApplicationController
   # GET /pieces/1.json
   def show; end
 
-  # GET /pieces/new
-  def new
-    @piece = Piece.new
-  end
-
-  # GET /pieces/1/edit
-  def edit; end
-
-  # POST /pieces
-  # POST /pieces.json
-  def create
-    @piece = Piece.new(piece_params)
-
-    respond_to do |format|
-      if @piece.save
-        format.html { redirect_to @piece, notice: 'Piece was successfully created.' }
-        format.json { render :show, status: :created, location: @piece }
-      else
-        format.html { render :new }
-        format.json { render json: @piece.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /pieces/1
-  # PATCH/PUT /pieces/1.json
-  def update
-    respond_to do |format|
-      if @piece.update(piece_params)
-        format.html { redirect_to @piece, notice: 'Piece was successfully updated.' }
-        format.json { render :show, status: :ok, location: @piece }
-      else
-        format.html { render :edit }
-        format.json { render json: @piece.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /pieces/1
-  # DELETE /pieces/1.json
-  def destroy
-    if @pieces.destroy
-      respond_to do |format|
-        format.html { redirect_to pieces_url, notice: 'Piece was successfully destroyed.' }
-        format.json { head :ok, status: :ok }
-      end
-    else
-      respond_to do |format|
-        format.html { redirect_to pieces_url }
-        format.json { head :no_content, status: :unprocessable_entity }
-      end
-    end
-  end
-
   private
 
   # Use callbacks to share common setup or constraints between actions.
   def set_piece
     @piece = Piece.find(params[:id])
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def piece_params
-    params.require(:piece).permit(%i[
-                                    description
-                                    gis_identifier
-                                    condition
-                                    evaluated
-                                    lat
-                                    long
-                                    type_id
-                                    division_id
-                                    suburb_id
-                                    model_id
-                                  ])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
@@ -122,10 +52,10 @@ class PiecesController < ApplicationController
                     gis_identifier
                     condition
                     evaluated
-                    type
-                    division
-                    suburb
-                    model
+                    type_id
+                    division_id
+                    suburb_id
+                    model_id
                     query
                     sort_by
                     sort_dir
--- app/views/divisions/_division.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! division, :id, :name, :created_at, :updated_at
-json.url division_url(division, format: :json)
